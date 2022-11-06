31d4e0414ae57b43f2d2062e5f69a009a84aa3e5
Ana Beatriz Kapps <anabeatrizkapps@ice.ufjf.br>
2022-11-06 20:07:27 -0300

    Exercicio 5 - BBC + arq de teste

---
+++ Exercicio5/index.py
@@ -0,0 +1,11 @@
+from BBS import BBS
+
+bits = BBS(11897, 13597)
+bits = bits.generateBits(20000)
+# print(bits)
+
+file = open("resultadoBitsBBS.txt", "a")
+
+for bit in bits:
+    file.write(str(bit)+"\n")
+file.close()
